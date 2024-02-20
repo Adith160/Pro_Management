db4c7ec1448903c2ef4f9c6e7b9502984aa2cb4c
Adith <adith@suvarnas.com>
2024-02-20 23:40:42 +0530

    Dashboard Changes

--- client/src/components/DashBoard/DashBoard.js
+++ client/src/components/DashBoard/DashBoard.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import styles from './DashBoard.module.css'
+import React from 'react';
+import styles from './DashBoard.module.css';
+import TaskCard from './TaskCard/TaskCard'
+import collapseIcon from '../../assets/icons/codicon_collapse-all.png'
+import addIcon from '../../assets/icons/Add.png'
 
 function DashBoard() {
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+
   return (
-    <div className={styles.mainContainer}></div>
-  )
+    <div className={styles.mainContainer}>
+        <div className={styles.topDiv}>
+            <span className={styles.headSpan}>Welcome! Kumar</span>
+            <span>{formattedDate}</span>
+        </div>
+
+        <div className={styles.headingDiv}>
+          <span className={styles.headSpan}>Board</span>
+          <select className={styles.weekSelection}>
+             <option value="option1" style={{border: "none"}}>Today</option>
+             <option value="option2" selected>This week</option>
+             <option value="option3">This month</option>
+          </select>
+        </div>
+
+        <div className={styles.mainDiv}>
+            <div className={styles.taskDiv}>
+                <div className={styles.taskHead}>
+                  <span className={styles.headSpan2}>Backlog</span> 
+                  <img src={collapseIcon} alt='collapse' className={styles.collapse}/>
+                </div>
+                <TaskCard />
+                <TaskCard />
+                <TaskCard />
+            </div>
+
+            <div className={styles.taskDiv}>
+                <div className={styles.taskHead}>
+                  <span className={styles.headSpan2}>To do</span> 
+                  <div>
+                      <img src={addIcon} alt='collapse'style={{width:"35%", height:"40%", marginRight:"20%", cursor:"pointer"}}/>
+                      <img src={collapseIcon} alt='collapse' style={{width:"40%", height:"60%",cursor:"pointer"}}/>
+                  </div>
+                </div>
+                <TaskCard />
+                <TaskCard />
+            </div>
+
+            <div className={styles.taskDiv}>
+                 <div className={styles.taskHead}>
+                    <span className={styles.headSpan2}>In progress</span> 
+                    <img src={collapseIcon} alt='collapse' className={styles.collapse}/>
+                </div>
+                  <TaskCard />
+                  <TaskCard />
+            </div>
+            <div className={styles.taskDiv}>
+                <div className={styles.taskHead}>
+                  <span className={styles.headSpan2}>Done</span> 
+                  <img src={collapseIcon} alt='collapse' className={styles.collapse}/>
+                </div>
+                  <TaskCard />
+                  <TaskCard />
+            </div>
+        </div>
+    </div>
+  );
 }
 
-export default DashBoard
+export default DashBoard;
---
+++ client/src/components/DashBoard/TaskCard/TaskCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './TaskCard.module.css'
+
+function TaskCard() {
+  return (
+    <div className={styles.taskCardDiv}>
+        hai
+        hello
+    </div>
+  )
+}
+
+export default TaskCard
