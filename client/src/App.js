406be9d568bdaae75308c40d636eb49652d352b7
Adith <adith@suvarnas.com>
2024-03-01 17:27:35 +0530

    Corrections

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
       <ToastContainer />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
